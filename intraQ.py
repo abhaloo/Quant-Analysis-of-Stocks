5472708c3b4d6cd77aca5a543e98e4d59e388676
Adeel <abhaloo@hotmail.com>
2018-08-16 12:53:22 +0300

    Save Debt/Equity Ratio to csv file
    Uses dataframe to output debt/equity ratio to TotalDebtEquitymrq.csv

--- intraQ.py
+++ intraQ.py
@@ -9,6 +9,8 @@ from pathlib import Path
 
 path = Path("C:/stockData/intraQuarter")
 
+
+# @param gather = value to be collected from files
 def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 	statspath = path / '_KeyStats'
 	
@@ -17,10 +19,10 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 	stock_list = [x[0] for x in os.walk(statspath)]
 	
 	#specify columns for data frame
-	df = pandas.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
+	df = pd.DataFrame(columns = ['Date','Unix','Ticker','Debt/Equity Ratio'])
 
 	#for every file in the directory
-	for each_dir in stock_list[1:]:
+	for each_dir in stock_list[1:5]:
 		
 		#list files in each directory
 		each_file = os.listdir(each_dir)
@@ -46,18 +48,26 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 				#open file with intention to read 'r'
 				source = open(full_file_path,'r').read()
 				
-				#gather = item to be collected
-				#elemment on the right of first split is the target value hence we use first element 
-				#then split again by table tag to collect element from left of the remaining paragraph 
+				
+				#Elemment on the right of first split is the target value hence we use first element 
+				#then split again by table tag to collect element from beginning/left of the remaining paragraph. 
+				#try-except is due to some files having an extra space or indent between table tags, 
+				#leading to index out of range errors.
 				try:
-					value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
+					
+					value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+					
+ 
+					df = df.append({'Date':date_stamp,'Unix':unix_time,'Ticker':ticker,'Debt/Equity Ratio':value},ignore_index = True)
+				
 				except Exception as e:
-					print(str(e))
-					value = float('nan')
+					#print(str(e))
+					pass
 				
-				print(ticker + ': ',value)
-			#time.sleep(15)
 				
+	save = gather.replace(' ','').replace(')','').replace('(','').replace('/','') + ('.csv')
+	print(save)
+	df.to_csv(save)				
 				
 				
 
