70f458e5287145909c557014c8e5996872f2ff18
Adeel <abhaloo@hotmail.com>
2018-08-16 12:22:31 +0300

    Initial Commit
    Initial Files include:
    
    #######predictInt.py
    using scikit learn and matplotlib to classify sample images of digits
    
    #####intraQ.py
    ongoing analysis of stock data from Yahoo Finance. Currently reached the point of traversing html files containing stock info on several companies and pulling the Total Debt/Equity ratio for each report they've published

---
+++ intraQ.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import os
+import time
+from datetime import datetime
+
+#use Path objects to adjust path format to match operating system format
+from pathlib import Path
+
+
+path = Path("C:/stockData/intraQuarter")
+
+def Key_Stats(gather = "Total Debt/Equity (mrq)"):
+	statspath = path / '_KeyStats'
+	
+	#for loop uses os.walk to list out all contents within a directory
+	#stock_list is file names within specified path 'statspath'
+	stock_list = [x[0] for x in os.walk(statspath)]
+	
+	#specify columns for data frame
+	df = pandas.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
+
+	#for every file in the directory
+	for each_dir in stock_list[1:]:
+		
+		#list files in each directory
+		each_file = os.listdir(each_dir)
+		
+		#read stock ticker/file name
+		ticker = each_dir.split('\\')[4]
+
+		#for every file that has been listed that is not empty
+		if len(each_file) > 0 :
+			
+			for file in each_file:
+				
+				#strip time from file names 
+				date_stamp = datetime.strptime(file,"%Y%m%d%H%M%S.html")
+
+				#produce in unix time
+				unix_time = time.mktime(date_stamp.timetuple())				
+				
+				#file path
+				full_file_path = each_dir + '/'+ file
+				#print(full_file_path)
+				
+				#open file with intention to read 'r'
+				source = open(full_file_path,'r').read()
+				
+				#gather = item to be collected
+				#elemment on the right of first split is the target value hence we use first element 
+				#then split again by table tag to collect element from left of the remaining paragraph 
+				try:
+					value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
+				except Exception as e:
+					print(str(e))
+					value = float('nan')
+				
+				print(ticker + ': ',value)
+			#time.sleep(15)
+				
+				
+				
+
+Key_Stats()
